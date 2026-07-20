644cc1620a7efed28aabbc8424215754a97f6e9a
moyinudeen <ameenabeessyedsm@gmail.com>
2026-07-20 14:28:53 +0530

    Create 344.(h.w)reverse string.java

---
+++ day-6/344.(h.w)reverse string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        
+        while (left < right) {
+            // Swap characters at left and right pointers
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            // Move pointers towards the middle
+            left++;
+            right--;
+        }
+    }
+}
