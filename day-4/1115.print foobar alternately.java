7197c23764f6e1fbf2f4be0ca4a6663e9fc104e8
moyinudeen <ameenabeessyedsm@gmail.com>
2026-07-20 13:28:05 +0530

    Create 1115.print foobar alternately.java

---
+++ day-4/1115.print foobar alternately.java
@@ -0,0 +1,37 @@
+import java.util.concurrent.Semaphore;
+
+class FooBar {
+    private int n;
+    private Semaphore fooSemaphore;
+    private Semaphore barSemaphore;
+
+    public FooBar(int n) {
+        this.n = n;
+        this.fooSemaphore = new Semaphore(1); // foo starts immediately
+        this.barSemaphore = new Semaphore(0); // bar waits
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            fooSemaphore.acquire(); // Wait for foo's turn (decrements permit to 0)
+            
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            
+            barSemaphore.release(); // Give bar its turn (increments permit to 1)
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            barSemaphore.acquire(); // Wait for bar's turn (decrements permit to 0)
+            
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            
+            fooSemaphore.release(); // Give foo its turn (increments permit to 1)
+        }
+    }
+}
