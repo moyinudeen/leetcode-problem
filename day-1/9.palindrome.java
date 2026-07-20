cfaccd8e0b712b0a87a5691b373968532476408e
moyinudeen <ameenabeessyedsm@gmail.com>
2026-07-20 09:45:23 +0530

    Add palindrome check implementation

---
+++ day-1/9.palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers are not palindromes
+        if (x < 0) {
+            return false;
+        }
+        
+        int reverse = 0;
+        int real = x;
+        
+        while (x != 0) {
+            int last = x % 10;
+            reverse = reverse * 10 + last;
+            x = x / 10; // Divide only once per loop
+        }
+        
+        return (real == reverse);
+    }
+}
