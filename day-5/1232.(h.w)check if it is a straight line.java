05f518fcc2953d8789768bbb47dbce957a96966c
moyinudeen <ameenabeessyedsm@gmail.com>
2026-07-20 14:10:21 +0530

    Create 1232.(h.w)check if it is a straight line.java

---
+++ day-5/1232.(h.w)check if it is a straight line.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        // Base variables using the first two points
+        int x0 = coordinates[0][0];
+        int y0 = coordinates[0][1];
+        int x1 = coordinates[1][0];
+        int y1 = coordinates[1][1];
+        
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        
+        // Check slope for all remaining points
+        for (int i = 2; i < coordinates.length; i++) {
+            int xi = coordinates[i][0];
+            int yi = coordinates[i][1];
+            
+            // Cross-multiplication to check slope consistency
+            if (dy * (xi - x0) != dx * (yi - y0)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
