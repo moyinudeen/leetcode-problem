919884ff826cfce51fef5ea3cda20a22586b87c3
moyinudeen <ameenabeessyedsm@gmail.com>
2026-07-21 09:58:34 +0530

    Clean up comments in Foo class methods
    Removed comments to clean up the code.

--- day-4/1114.print in order.java
+++ day-4/1114.print in order.java
@@ -6,31 +6,22 @@ class Foo {
     private final CountDownLatch latch2;
 
     public Foo() {
-        // Initialize latches with a count of 1
         latch1 = new CountDownLatch(1);
         latch2 = new CountDownLatch(1);
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
-        // Release the first latch to allow second() to proceed
         latch1.countDown();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        // Wait until first() calls latch1.countDown()
         latch1.await();
-        // printSecond.run() outputs "second". Do not change or remove this line.
         printSecond.run();
-        // Release the second latch to allow third() to proceed
         latch2.countDown();
     }
-
     public void third(Runnable printThird) throws InterruptedException {
-        // Wait until second() calls latch2.countDown()
         latch2.await();
-        // printThird.run() outputs "third". Do not change or remove this line.
         printThird.run();
     }
 }
