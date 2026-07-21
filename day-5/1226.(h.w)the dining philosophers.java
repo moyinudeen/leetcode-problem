b66d7f6cd4cd6488645650a6de28298c7f5d36af
moyinudeen <ameenabeessyedsm@gmail.com>
2026-07-21 10:31:15 +0530

    Clean up comments in DiningPhilosophers class
    Removed comments explaining semaphore usage and dining process steps.

--- day-5/1226.(h.w)the dining philosophers.java
+++ day-5/1226.(h.w)the dining philosophers.java
@@ -1,10 +1,7 @@
 import java.util.concurrent.Semaphore;
 
 class DiningPhilosophers {
-    
-    // Array of 5 semaphores representing each individual fork
     private final Semaphore[] forks;
-    // Semaphore to limit the number of philosophers eating concurrently to 4
     private final Semaphore bouncer;
 
     public DiningPhilosophers() {
@@ -14,8 +11,6 @@ class DiningPhilosophers {
         }
         bouncer = new Semaphore(4);
     }
-
-    // call the run() method of any runnable to execute its code
     public void wantsToEat(int philosopher,
                            Runnable pickLeftFork,
                            Runnable pickRightFork,
@@ -25,26 +20,16 @@ class DiningPhilosophers {
         
         int leftFork = philosopher;
         int rightFork = (philosopher + 1) % 5;
-
-        // Step 1: Limit total concurrent dining attempts to 4 to prevent deadlock
         bouncer.acquire();
-
-        // Step 2: Acquire both forks safely
         forks[leftFork].acquire();
         forks[rightFork].acquire();
-
-        // Step 3: Execute actions
         pickLeftFork.run();
         pickRightFork.run();
         eat.run();
         putLeftFork.run();
         putRightFork.run();
-
-        // Step 4: Release both forks
         forks[leftFork].release();
         forks[rightFork].release();
-
-        // Step 5: Leave the table
         bouncer.release();
     }
 }
