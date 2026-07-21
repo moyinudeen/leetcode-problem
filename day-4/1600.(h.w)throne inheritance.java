a23b680e7948f1d7acf24ed51077d32db5b38ea5
moyinudeen <ameenabeessyedsm@gmail.com>
2026-07-21 10:03:20 +0530

    Update 1600.(h.w)throne inheritance.java

--- day-4/1600.(h.w)throne inheritance.java
+++ day-4/1600.(h.w)throne inheritance.java
@@ -1,47 +1,34 @@
 import java.util.*;
 
 class ThroneInheritance {
-    // Adjacency list to represent the family tree
     private Map<String, List<String>> familyTree;
-    // Set to keep track of dead family members
     private Set<String> deadMembers;
-    // The starting point of the family tree
     private String kingName;
-
     public ThroneInheritance(String kingName) {
         this.kingName = kingName;
         this.familyTree = new HashMap<>();
         this.deadMembers = new HashSet<>();
-        // Initialize the king in the tree
         this.familyTree.put(kingName, new ArrayList<>());
     }
     
     public void birth(String parentName, String childName) {
-        // Add the child to the parent's list of children
         this.familyTree.get(parentName).add(childName);
-        // Initialize an empty list of children for the newborn
         this.familyTree.put(childName, new ArrayList<>());
     }
-    
     public void death(String name) {
-        // Mark the person as dead
         this.deadMembers.add(name);
     }
     
     public List<String> getInheritanceOrder() {
         List<String> order = new ArrayList<>();
-        // Perform preorder traversal starting from the king
         dfs(kingName, order);
         return order;
     }
 
     private void dfs(String current, List<String> order) {
-        // If the current person is alive, add them to the order
         if (!deadMembers.contains(current)) {
             order.add(current);
         }
-        
-        // Traverse all children in the order of their birth
         List<String> children = familyTree.get(current);
         if (children != null) {
             for (String child : children) {
