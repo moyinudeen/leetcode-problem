72c9d5f7b1305c7ba4818145f547829f736b5e9f
moyinudeen <ameenabeessyedsm@gmail.com>
2026-07-21 09:32:59 +0530

    Refactor twoSum method for clarity
    Refactor twoSum method by removing comments and cleaning up the code.

--- day-2/1.two sum.java
+++ day-2/1.two sum.java
@@ -3,22 +3,22 @@ import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Map to store value and its corresponding index
+        
         Map<Integer, Integer> numMap = new HashMap<>();
         
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             
-            // If the complement exists in the map, we found the pair
+            r
             if (numMap.containsKey(complement)) {
                 return new int[] { numMap.get(complement), i };
             }
             
-            // Otherwise, add the current number and its index to the map
+            
             numMap.put(nums[i], i);
         }
         
-        // Return an empty array if no solution is found (though the problem guarantees one)
+        
         return new int[] {};
     }
 }
